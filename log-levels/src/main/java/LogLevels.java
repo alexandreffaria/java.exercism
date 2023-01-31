e2672fc52ca0fea533b6f139b7786361d9841fc1
Alexandre Faria <alexandreffaria@me.com>
2023-01-31 03:00:37 -0300

    strings

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,19 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        String noTrail = logLine.trim();
+        String[] parts = noTrail.split(": ");
+        return parts[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        String noTrail = logLine.trim();
+        String[] parts = noTrail.split(": ");
+        return parts[0].trim().substring(1, parts[0].length() - 1).toLowerCase();
+
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 }
